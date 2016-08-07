95517c4e54f11d7960bec1c9075b4bacc1319b3f
Olivier Midy <osmidy@mit.edu>
2016-08-06 22:37:11 -0700

    Test code.

---
+++ Musync/Musync/BlyncHelper.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blynclight;
+using System.Threading;
+
+namespace Musync
+{
+    /// <summary>
+    ///  Representation of a particulary Blynclight device. Helper functions
+    ///  are included to extend the functionality of the Blynclight, 
+    ///  particularly BLYNCUSB10 and BLYNCUSB17/20 models
+    /// </summary>
+    public class BlyncHelper
+    {
+        public enum LightColor
+        {
+            Red, Green, Blue, Cyan, Magenta, Yellow, Orange, White
+        }
+        /// <summary>
+        /// Creates a pulse effect on the desired Blynclight
+        /// </summary>
+        /// <param name="controller">The controller for connected devices</param>
+        /// <param name="color">Current color of the light during pulse</param>
+        /// <param name="index">Index of the targeted device</param>
+        /// <param name="length">Total length of the pulse in milliseconds</param>
+        public static void Pulse(BlynclightController controller, LightColor color, int index, int speed)
+        {
+            var sleepTime = speed / 2;
+
+            controller.ResetLight(index);
+            Thread.Sleep(sleepTime);
+            SetColor(controller, color, index);
+        }
+
+        public static void SetColor(BlynclightController controller, LightColor color, int index)
+        {
+            switch(color)
+            {
+                case (LightColor.Red):
+                    controller.TurnOnRedLight(index);
+                    break;
+                case (LightColor.Green):
+                    controller.TurnOnGreenLight(index);
+                    break;
+                case (LightColor.Blue):
+                    controller.TurnOnBlueLight(index);
+                    break;
+                case (LightColor.Cyan):
+                    controller.TurnOnCyanLight(index);
+                    break;
+                case (LightColor.Magenta):
+                    controller.TurnOnMagentaLight(index);
+                    break;
+                case (LightColor.Yellow):
+                    controller.TurnOnYellowLight(index);
+                    break;
+                case (LightColor.Orange):
+                    controller.TurnOnOrangeLight(index);
+                    break;
+                case (LightColor.White):
+                    controller.TurnOnWhiteLight(index);
+                    break;
+            }
+        }
+    }
+}
---
+++ Musync/Musync/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Blynclight;
+using System.Threading;
+
+namespace Musync
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var color = BlyncHelper.LightColor.Red;
+            var light = new BlynclightController();
+            var numDevices = light.InitBlyncDevices();
+            for (int i = 0; i < numDevices; i++)
+            {
+                var sleepTime = 1000;
+                light.TurnOnRedLight(i);
+                Thread.Sleep(sleepTime);
+                BlyncHelper.Pulse(light,color, i, 25);
+                
+            }
+        }
+    }
+}
