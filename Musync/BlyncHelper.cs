abbe84c1b36e2166fde6423e14bef50729218641
Olivier S. Midy <osmidy@gmail.com>
2016-12-21 01:25:23 -0500

    Fixed file structure (again)

---
+++ Musync/BlyncHelper.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blynclight;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Musync
+{
+    /// <summary>
+    /// Representation of possible Blynclight color values
+    /// </summary>
+    public enum LyncColor
+    {
+        White = 0, Yellow, Green, Cyan, Blue, Magenta, Red
+    }
+
+    /// <summary>
+    ///  Wrapper class for BlynclightController to extend the functionality of the Blynclight, 
+    ///  particularly BLYNCUSB10 and BLYNCUSB17/20 models
+    /// </summary>
+    public class BlyncHelper
+    {
+        /// <summary>
+        /// Controller for this instance
+        /// </summary>
+        private readonly BlynclightController controller;
+        
+        /// <summary>
+        /// Number of connected Blynclights
+        /// </summary>
+        private readonly int numDevices;
+
+        /// <summary>
+        /// Current color of device at index 0
+        /// </summary>
+        private LyncColor color;
+
+        private double lastPulseTime;
+        private int minPulseDt = 300;
+
+        /// <summary>
+        /// Creates a Blynchelper instance
+        /// </summary>
+        /// <param name="controller">BlynclightController wrapped by this instance</param>
+        public BlyncHelper(BlynclightController controller)
+        {
+            this.controller = controller;
+            this.numDevices = controller.InitBlyncDevices();
+            this.lastPulseTime = DateTime.UtcNow.TimeOfDay.TotalMilliseconds;
+        }
+
+
+        /// <summary>
+        /// Controller for this instance
+        /// </summary>
+        public BlynclightController Controller
+        {
+            get { return this.controller; }
+        }
+
+        public int NumDevices
+        {
+            get { return this.numDevices; }
+        }
+
+        public LyncColor Color
+        {
+            get { return this.color;    }
+            set { this.SetColor(value); }
+        }
+        
+        /// <summary>
+        /// Creates a pulse effect on the desired Blynclight
+        /// </summary>
+        /// <param name="color">Current color of the light during pulse</param>
+        /// <param name="length">Total length of the pulse in milliseconds</param>
+        public void Pulse(LyncColor color, int length = 40)
+        {
+            if (length < 0) return;
+
+            var now = DateTime.UtcNow.TimeOfDay.TotalMilliseconds;
+            double dt = now - this.lastPulseTime;
+
+            if (dt < this.minPulseDt) return;
+
+            this.lastPulseTime = now;
+
+            this.controller.ResetLight(0);
+            Thread.Sleep(length);
+            this.SetColor(color);
+        }
+
+        public void Pulse()
+        {
+            this.Pulse(this.color);
+        }
+
+        public void SetColorAll(LyncColor color)
+        {
+            for (int i = 0; i < this.NumDevices; i++)
+            {
+                this.SetColor(color, i);
+            }
+        }
+
+        public void TurnOff()
+        {
+            for (int i =  0; i < this.NumDevices; i++)
+            {
+                this.controller.ResetLight(i);
+            }
+        }
+
+
+        /// <summary>
+        /// Sets the color of the desired Blynclight
+        /// </summary>
+        /// <param name="color">Color to be set on Blynclight</param>
+        /// <param name="index">Index of the targeted device</param>
+        public void SetColor(LyncColor color, int index = 0)
+        {
+            switch(color)
+            {
+                case (LyncColor.White):
+                    controller.TurnOnWhiteLight(index);
+                    break;
+                case (LyncColor.Red):
+                    controller.TurnOnRedLight(index);
+                    break;
+                case (LyncColor.Yellow):
+                    controller.TurnOnYellowLight(index);
+                    break;
+                case (LyncColor.Green):
+                    controller.TurnOnGreenLight(index);
+                    break;
+                case (LyncColor.Cyan):
+                    controller.TurnOnCyanLight(index);
+                    break;
+                case (LyncColor.Blue):
+                    controller.TurnOnBlueLight(index);
+                    break;                
+                case (LyncColor.Magenta):
+                    controller.TurnOnMagentaLight(index);
+                    break;
+            }
+
+            this.color = color;
+        }
+    }
+}
---
+++ Musync/Musync.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Musync
+{
+    partial class Musync
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Musync));
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(176, 96);
+            this.btnPlay.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(212, 90);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // Musync
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1364, 1237);
+            this.Controls.Add(this.btnPlay);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Musync";
+            this.Text = "Musync";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlay;
+    }
+}
+
